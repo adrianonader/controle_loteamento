d414078250fcd70776c3ab04d27e149187c96272
Prem Prakash <prakash2ji@gmail.com>
2022-03-09 11:06:52 -0300

    comment broken migrations

--- db/migrate/20220307023204_rename_status_to_vendido_on_lotes.rb
+++ db/migrate/20220307023204_rename_status_to_vendido_on_lotes.rb
@@ -1,5 +1,5 @@
 class RenameStatusToVendidoOnLotes < ActiveRecord::Migration[7.0]
   def change
-    rename_column(:lotes, :status, :vendido)
+    # rename_column(:lotes, :status, :vendido)
   end
 end
